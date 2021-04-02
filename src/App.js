b062c9b2681d45e8fb290ce3cb24dd1bc8586667
Kunal <kdshah6593@gmail.com>
2021-04-02 13:52:19 -0400

    header and footer components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         App
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function Header() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        The Sports Report
+                    </Typography>
+                    <IconButton edge="end" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
