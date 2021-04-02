a9a8b852025a2c31badfcd004256f3c8338eaa86
Kunal <kdshah6593@gmail.com>
2021-04-02 14:40:00 -0400

    home component, importing login sign up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/Signup';
 
 class App extends React.Component {
   render() {
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                Home Page for Player and Team Container
+            </div>
+        )
+    }
+}
+
+export default Home;
