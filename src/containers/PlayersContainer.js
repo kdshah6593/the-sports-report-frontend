7397151ae2c9283c767b73fcf1afcb63e302234e
Kunal <kdshah6593@gmail.com>
2021-04-02 15:43:13 -0400

    master detail pattern planning

--- src/containers/PlayersContainer.js
+++ src/containers/PlayersContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Player from '../components/players/Player';
+import Players from '../components/players/Players';
 
 class PlayersContainer extends React.Component {
     render() {
         return (
             <div>
-                This is the player container
+                This is the player container.
+                It will load up all the current favorite players of the user as links on left hand side.
+                - this is done via Players Component, which will map over players and create each one as a link
+                - Player Component will be display of articles as cards
+                Then if a link is clicked, the detail (articles) will show up on right hand side via route
+                <Switch>
+                    <Route path="/players/:id" component={Player} />
+                </Switch>
             </div>
         )
     }
