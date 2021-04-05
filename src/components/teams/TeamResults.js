204d7b10d9025879d63805c7d2365d3fee9191ec
Kunal <kdshah6593@gmail.com>
2021-04-05 16:19:14 -0400

    adjust function syntax for functional component

--- src/components/teams/TeamResults.js
+++ src/components/teams/TeamResults.js
@@ -11,30 +11,28 @@ const TeamResults = (props) => {
     
     const handleOptionChange = (event) => {
         console.log(event.target.value)
-        this.setState({
-            selectedOption: event.target.value
-        })
+        setSelectedOption(event.target.value)
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const selectedTeam = this.state.searchResults[this.state.selectedOption]
+        const selectedTeam = searchResults[selectedOption]
         // this will send a fetch post request to the backend
         // create the object here with the information needed for backend like name, sport to be stringifed
     }
 
     const mapSearchResults = () => {
-        if (this.props.searchResults === null) {
+        if (props.searchResults === null) {
             return "No Results, please try again."
         } else {
-            this.props.searchResults.map((team, index) => <label key={team.idTeam}><input type="radio" name="team" value={index} onChange={this.handleOptionChange}/>{team.strTeam} - {team.strLeague}</label>)
+            props.searchResults.map((team, index) => <label key={team.idTeam}><input type="radio" name="team" value={index} onChange={handleOptionChange}/>{team.strTeam} - {team.strLeague}</label>)
         }
     }
 
-    const teams = this.mapSearchResults()
+    const teams = mapSearchResults()
     return (
         <div>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 {teams}
                 <input type="submit" value="Add Team" />
             </form>
