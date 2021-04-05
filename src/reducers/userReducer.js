5ec1526efcf8f96046323e5522dbab5d44a7ce91
Kunal <kdshah6593@gmail.com>
2021-04-05 16:13:20 -0400

    update state when new player added

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,14 @@
 export default function userReducer(state = {
-    user: [],
+    user: {},
     currentView: []
 }, action) {
     switch (action.type) {
         case 'ADD_USER':
             return state //this will establish the current user on log in
+        case 'ADD_PLAYER':
+            return {...state, user: action.payload} //this will update the state user to reflect added player
+        case 'ADD_TEAM':
+            return state //this will udpate the state user to reflect added team
         case 'CHANGE_VIEW':
             return state //this will establish what player or team is being viewed
         default:
