cdaa474d6fc52adcaf9bac5893f02544945859fe
Kunal <kdshah6593@gmail.com>
2021-04-06 15:26:47 -0400

    set up player component to appear with details

--- src/components/players/Player.js
+++ src/components/players/Player.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class Player extends React.Component {
+    state = {
+        name: this.props.player.name
+    }
+    
     render() {
         return (
             <div>
-
+                <p>{this.state.name}</p>
             </div>
         )
     }
 }
 
 export default Player;
+
+
+// this component should display the articles for the player that is passed through props and use async fetch request
