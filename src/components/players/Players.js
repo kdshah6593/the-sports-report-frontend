7a15e8acf9dc3ea0fed85bdedb88a2643d382d9d
Kunal <kdshah6593@gmail.com>
2021-04-06 15:27:29 -0400

    create master detail pattern to change player shown when differnt player clicked

--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Player from './Player';
+
 
 class Players extends React.Component {
+    state = {
+        selectedPlayer: {}
+    }
+    
+    selectPlayer = (player) => {
+        console.log("ive been clicked");
+        this.setState({ 
+            selectedPlayer: player
+        })
+    }
+
     render() {
+        const players = this.props.players.map( (player, index) => <li key={index} onClick={() => this.selectPlayer(player)}>{player.name}</li>);
         return (
             <div>
+                <div>
+                    <h4>Player List</h4>
+                    <ul>
+                        {players}
+                    </ul>
+                </div>
+                <div>
+                    <h4>Player Details</h4>
+                    {this.state.selectedPlayer ? <Player key={this.state.selectedPlayer.sportsDBId} player={this.state.selectedPlayer} /> : "No Player Selected Yet" }
+                </div>
 
             </div>
         )
@@ -11,3 +35,5 @@ class Players extends React.Component {
 }
 
 export default Players;
+
+// this component will render a mapped list of user player
