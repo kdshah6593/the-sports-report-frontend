7f332b61743b84d3a947863846dc054ae6cb47f8
Kunal <kdshah6593@gmail.com>
2021-04-06 19:01:47 -0400

    add logged in boolean to state

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,16 +1,14 @@
 export default function userReducer(state = {
     user: {},
-    currentView: []
+    loggedIn: false
 }, action) {
     switch (action.type) {
         case 'ADD_USER':
-            return {...state, user: action.payload} //this will establish the current user on log in
+            return {...state, user: action.payload, loggedIn: true}; //this will establish the current user on log in
         case 'ADD_PLAYER':
             return {...state, user: action.payload} //this will update the state user to reflect added player
         case 'ADD_TEAM':
             return {...state, user: action.payload} //this will udpate the state user to reflect added team
-        case 'CHANGE_VIEW':
-            return state //this will establish what player or team is being viewed
         default:
             return state
     }
