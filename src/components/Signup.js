aa424b588a7c97ba5ee818174352a158479b3d91
Kunal <kdshah6593@gmail.com>
2021-04-06 17:47:57 -0400

    remove unneeded form elements

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,16 +3,14 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { Link } from 'react-router-dom'
+import { Link as RouteLink } from 'react-router-dom'
 
 function Copyright() {
   return (
@@ -119,12 +117,6 @@ export default function SignUp() {
                 autoComplete="current-password"
               />
             </Grid>
-            {/* <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid> */}
           </Grid>
           <Button
             type="submit"
@@ -137,9 +129,9 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link to={`/login`} variant="body2">
+              <RouteLink to={`/login`} variant="body2">
                 Already have an account? Sign in
-              </Link>
+              </RouteLink>
             </Grid>
           </Grid>
         </form>
