e1932a807f3727bc83441ac79688cc37de2cec4d
Kunal <kdshah6593@gmail.com>
2021-04-07 16:54:08 -0400

    css grid style for master detail patten

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Team from './Team';
 import { connect } from 'react-redux';
 import { fetchArticles } from '../../actions/fetchArticles'
 
-class Teams extends React.Component {
-    state = {
-        selectedTeam: ""
-    }
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-    selectTeam = (team) => {
-        console.log("ive been clicked");
-        this.setState({
-            selectedTeam: team
-        })
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+const Teams = (props) => {
+    const classes = useStyles();
+
+    const [selectedTeam, useSelectedTeam] = useState("")
+
+    const SelectTeam = (team) => {
+        useSelectedTeam(team)
         this.props.fetchArticles(team)
     }
     
-    render() {
-        const teams = this.props.teams.map( (team, index) => <li key={index} onClick={() => this.selectTeam(team)}>{team.name}</li>);
-        return (
-            <div>
-                <div>
-                    <h4>Team List</h4>
-                    <ul>
-                        {teams}
-                    </ul>
-                </div>
-                <div>
-                    <h4>Team Details</h4>
-                    {this.state.selectedTeam ? <Team key={this.state.selectedTeam.sportsDBId} team={this.state.selectedTeam} /> : <p>No Team Selected</p> }
-                </div>
-            </div>
-        )
-    }
+    const teams = props.teams.map( (team, index) => <li key={index} onClick={() => SelectTeam(team)}>{team.name}</li>);
+    
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={1}>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <h3>Team List</h3>
+                        <ul>
+                            {teams}
+                        </ul>
+                    </Paper>
+                </Grid>
+                <Grid item xs={9}>
+                    <Paper className={classes.paper}>
+                        <h3>Team Details</h3>
+                        {selectedTeam ? <Team key={selectedTeam.sportsDBId} team={selectedTeam} /> : <p>No Team Selected</p> }
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => {
