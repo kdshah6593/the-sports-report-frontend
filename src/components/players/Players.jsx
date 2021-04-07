bf6bb1a7fe54d6a7c2b9fb5ba4a62e71ea5fcbb9
Kunal <kdshah6593@gmail.com>
2021-04-07 16:46:15 -0400

    css grid style for master detail pattern

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -3,27 +3,51 @@ import Player from './Player';
 import { connect } from 'react-redux';
 import { fetchArticles } from '../../actions/fetchArticles';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
 const Players = (props) => {
+    const classes = useStyles();
+    
     const [selectedPlayer, useSelectedPlayer] = useState("")
     
-    const selectPlayer = (player) => {
+    const SelectPlayer = (player) => {
         useSelectedPlayer(player)
-        this.props.fetchArticles(player)
+        props.fetchArticles(player)
     }
 
-    const players = props.players.map( (player, index) => <li key={index} onClick={() => selectPlayer(player)}>{player.name}</li>);
+    const players = props.players.map( (player, index) => <li key={index} onClick={() => SelectPlayer(player)}>{player.name}</li>);
+    
     return (
-        <div>
-            <div>
-                <h4>Player List</h4>
-                <ul>
-                    {players}
-                </ul>
-            </div>
-            <div>
-                <h4>Player Details</h4>
-                {selectedPlayer ? <Player key={selectedPlayer.sportsDBId} player={selectedPlayer} /> : <p>No Player Selected</p> }
-            </div>
+        <div className={classes.root}>
+            <Grid container spacing={1}>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <h3>Player List</h3>
+                        <ul>
+                            {players}
+                        </ul>
+                    </Paper>
+                </Grid>
+                <Grid item xs={9}>
+                    <Paper className={classes.paper}>
+                        <h3>Player Details</h3>
+                        {selectedPlayer ? <Player key={selectedPlayer.sportsDBId} player={selectedPlayer} /> : <p>No Player Selected</p> }
+                    </Paper>
+                </Grid>
+            </Grid>
         </div>
     )
 }
