3276765c23c72e0cabf2d277186501c3bd36943d
Kunal <kdshah6593@gmail.com>
2021-04-07 12:49:58 -0400

    action to fetch articles

---
+++ src/actions/fetchArticles.jsx
@@ -0,0 +1,24 @@
+export function fetchArticles(player) {
+    const url = 'https://api.currentsapi.services/v1/search?language=en&category=sports'
+    const apikey = `&apiKey=${process.env.REACT_APP_CURRENTS_API_KEY}`
+    const playerName = `&keywords=${player.name.replace(" ", "%20")}`
+    return (dispatch) => {
+        dispatch({ type: 'START_ADDING_ARTICLES_REQUEST' });
+        fetch(url+playerName+apikey)
+            .then(response => response.json())
+            .then(data => dispatch({ type: 'ADD_ARTICLES', payload: data.news}))
+    }
+}
+
+// fetchArticles = () => {
+//     const url = 'https://api.currentsapi.services/v1/search?language=en&category=sports'
+//     const apikey = `&apiKey=${process.env.REACT_APP_CURRENTS_API_KEY}`
+//     const playerName = `&keywords=${this.state.name.replace(" ", "%20")}`
+//     fetch(url+playerName+apikey)
+//     .then(response => response.json())
+//     .then(data => {
+//         this.setState({
+//             articles: data.news
+//         })
+//     })
+// }
