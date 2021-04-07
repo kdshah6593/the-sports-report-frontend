b14cb9abb342020fe53ace73031d516e6f8b4cd6
Kunal <kdshah6593@gmail.com>
2021-04-07 12:49:05 -0400

    add cases for adding articles to store, action for loading

--- src/reducers/userReducer.jsx
+++ src/reducers/userReducer.jsx
@@ -1,6 +1,8 @@
 const initialState = {
     user: {},
-    loggedIn: false
+    loggedIn: false,
+    articles: [],
+    requesting: false
 }
 
 export default function userReducer(state = initialState, action) {
@@ -13,6 +15,10 @@ export default function userReducer(state = initialState, action) {
             return {...state, user: action.payload} //this will udpate the state user to reflect added team
         case 'LOGOUT':
             return { ...initialState }
+        case 'START_ADDING_ARTICLES_REQUEST':
+            return {...state, requesting: true }
+        case 'ADD_ARTICLES':
+            return {...state, articles: action.payload, requesting: false}
         default:
             return state
     }
