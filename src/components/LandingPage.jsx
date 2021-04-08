ccbe03a6e523569cf3f7ed0f6e270890fb640f4a
Kunal <kdshah6593@gmail.com>
2021-04-08 16:23:01 -0400

    css, image, text

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,6 +4,8 @@ import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import '../Styles.css';
 import { makeStyles } from '@material-ui/core/styles';
+import sportsPic from '../images/sports-magazine.png';
+import BeenhereSharpIcon from '@material-ui/icons/BeenhereSharp';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,12 +22,25 @@ const LandingPage = () => {
     const classes = useStyles();
     return (
         <div>
-            <h1 className="center textColor">The Sports Report</h1>
+            <h1 className="center textColor landingTitle">The Sports Report</h1>
             <div className={classes.root}>
-                <ButtonGroup>
+                <ButtonGroup size="large">
                     <Button variant="contained" style={{ background: '#E09F3E', color: 'white'}}><Link to={`/login`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Login</Link></Button>
                     <Button variant="contained" style={{ background: '#E09F3E', color: 'white'}}><Link to={`/signup`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Signup</Link></Button>
                 </ButtonGroup>
+            </div>
+            <br></br><br></br>
+            <div className="landingContainer">
+                <div>
+                    <p className="textColor landingText"><span><BeenhereSharpIcon /></span> <em>Add Your Favorite Players and Teams</em></p>
+                    <p className="textColor landingText"><span><BeenhereSharpIcon /></span> <em>No More Searching for Articles</em></p>
+                    <p className="textColor landingText"><span><BeenhereSharpIcon /></span> <em>Articles Curated Toward Your Favorites</em></p>
+                    <p className="textColor landingText"><span><BeenhereSharpIcon /></span> <em>Get the Latest Articles They Are Mentioned In</em></p>
+                </div>
+                <div>
+                    <img class="imgSize" src={sportsPic} alt="" />
+                </div>
+
             </div>
         </div>
     )
