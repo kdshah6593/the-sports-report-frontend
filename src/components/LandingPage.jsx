7d0e98caa4c21e6d8020632cd828a45ec8afcfe6
Kunal <kdshah6593@gmail.com>
2021-04-08 15:10:59 -0400

    button grouping alignment and color

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import '../Styles.css';
+import { makeStyles } from '@material-ui/core/styles';
 
-class LandingPage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>The Sports Report</h1>
-                <div>
-                    <Button variant="contained" color="primary"><Link to={`/login`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Login</Link></Button>
-                </div>
-                <br></br>
-                <div>
-                    <Button variant="contained" color="primary"><Link to={`/signup`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Signup</Link></Button>
-                </div>
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+const LandingPage = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            <h1 className="center textColor">The Sports Report</h1>
+            <div className={classes.root}>
+                <ButtonGroup>
+                    <Button variant="contained" style={{ background: '#E09F3E', color: 'white'}}><Link to={`/login`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Login</Link></Button>
+                    <Button variant="contained" style={{ background: '#E09F3E', color: 'white'}}><Link to={`/signup`} style={{ color: 'inherit', textDecoration: 'inherit'}}>Signup</Link></Button>
+                </ButtonGroup>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default LandingPage;
