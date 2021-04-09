408cc975f0b334cfc6cbe182482952e9efbfff4a
Kunal <kdshah6593@gmail.com>
2021-04-08 20:47:12 -0400

    delete functionality, event listener, connection to backend

--- src/components/players/PlayerList.jsx
+++ src/components/players/PlayerList.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { List, ListItem, ListItemText, IconButton } from '@material-ui/core'
+import { useHistory } from 'react-router';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,19 +13,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function PlayerList(props) {
+const PlayerList = (props) => {
   const classes = useStyles();
   const [selectedIndex, setSelectedIndex] = React.useState("");
+
+  let history = useHistory()
 
   const handleListItemClick = (event, index, player) => {
     setSelectedIndex(index);
     props.SelectPlayer(player)
   };
 
+  const handleDeleteClick = (event, player) => {
+    console.log("im clicked")
+    console.log(player)
+    const dataToSend = {
+      sportsDBId: player.sportsDBId
+    }
+    fetch(`http://localhost:3001/api/v1/players/${player.id}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
+            },
+            body: JSON.stringify(dataToSend)
+        })
+        .then(response => response.json())
+        .then(updatedUser => {
+            console.log(updatedUser)
+            props.deletePlayer(updatedUser.data)
+            history.push("/players")
+        })
+  }
+
   const players = props.players.map((player, index) => (
-      <ListItem button selected={selectedIndex === index} onClick={(event) => handleListItemClick(event, index, player)} key={index}>
-          <ListItemText primary={player.name} className="subText" />
-          <IconButton edge="end">
+      <ListItem button selected={selectedIndex === index} key={index}>
+          <ListItemText primary={player.name} className="subText" onClick={(event) => handleListItemClick(event, index, player)} />
+          <IconButton edge="end" onClick={(event) => handleDeleteClick(event, player)}>
               <DeleteIcon />
           </IconButton>
       </ListItem>
@@ -37,3 +64,11 @@ export default function PlayerList(props) {
     </div>
   );
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+      deletePlayer: userData => dispatch({type: 'DELETE_PLAYER', payload: userData })
+  }
+}
+
+export default connect(null, mapDispatchToProps)(PlayerList);
